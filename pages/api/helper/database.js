67f963048f26edcc4c8f8e2f078df0a49c4640c1
Angelo Esmeralda <angeloje@macbookangelo.local>
2022-12-08 18:31:21 -0800

    update

--- pages/api/helper/database.js
+++ pages/api/helper/database.js
@@ -1,6 +1,11 @@
 const config = require("../config.json");
 
+const cluster = process.env.cluster;
+const username = process.env.username;
+const password = process.env.password;
+const name = process.env.name;
+
 export const database = {
-  url: `mongodb+srv://${config.username}:${config.password}@${config.cluster}/?retryWrites=true&w=majority`,
-  name: config.name,
+  url: `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority`,
+  name: name,
 };
