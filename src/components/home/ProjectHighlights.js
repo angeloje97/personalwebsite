388fb57d8a6c30849d5ddbeb225c5cb9880757df
Angelo Esmeralda <angeloje@macbookangelo.local>
2023-03-29 16:15:01 -0700

    update

--- src/components/home/ProjectHighlights.js
+++ src/components/home/ProjectHighlights.js
@@ -2,19 +2,23 @@ import React from "react";
 import CardHeader from "../cards/CardHeader";
 import Image from "../elements/Image";
 import style from "./ProjectHighlights.module.css";
+import List from "../elements/List";
 
 const projects = [
   {
     name: "Architome",
+    thumbnail: "/resources/pictures/Architome1.png",
     description:
       "A game that I've put a lot of heart and soul into since 2021. An action RPG in which the player is able to micromanage " +
-      "5 party members through dungeons.",
+      "5 party members through dungeons. Mechanics include enemy behavior, inventory and item management, party management, quest systems," +
+      "procedural dungeons, guild systems, and much more.",
     links: [
       {
         name: "More",
         url: "projects/select?id=6344b5ba98d645313a2545b9",
       },
     ],
+    tech: ["Unity", "C#"],
   },
 ];
 
@@ -32,13 +36,16 @@ const ProjectHighlights = (props) => {
 
 const Project = ({ projectData: data }) => {
   const links = data.links.map((link) => <a href={link.url}>{link.name}</a>);
+
+  const technologyUsed = data.tech.map((tech) => <li key={tech}>{tech}</li>);
   return (
     <CardHeader
       className={style.projectCard}
-      header={<Image src="/resources/pictures/Architome1.png"></Image>}
+      header={<Image src={data.thumbnail}></Image>}
     >
       <h3>{data.name}</h3>
       <p>{data.description}</p>
+      <List>{technologyUsed}</List>
       <div>{links}</div>
     </CardHeader>
   );
