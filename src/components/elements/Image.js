8d6c865323e41ed58b50f5929467261f36c7fcac
Angelo Esmeralda <angeloje@macbookangelo.local>
2023-04-01 16:48:48 -0700

    Changed img element to Image from nextjs

--- src/components/elements/Image.js
+++ src/components/elements/Image.js
@@ -7,7 +7,7 @@ const NewImage = (props) => {
 
   const finalClass = styleGroup(style.image, classtype, className, style);
 
-  return <img {...props} src={src} className={finalClass} />;
+  return <Image {...props} src={src} className={finalClass} priority />;
 };
 
 export default NewImage;
