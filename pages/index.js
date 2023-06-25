4704645dc235d8fbea65c40bfc9a95375973f2b9
angeloje <angeloje97@outlook.com>
2023-06-25 15:00:36 -0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,13 @@ import ClipBoard from "../src/components/modals/ClipBoard";
 import ConfirmPrompt from "../src/components/modals/ConfirmPrompt";
 import HomeGrid from "../src/components/home/HomeGrid";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import React from "react";
 const Index = () => {
   return (
     <React.Fragment>
+      <Analytics />
       <HomeGrid></HomeGrid>
     </React.Fragment>
   );
